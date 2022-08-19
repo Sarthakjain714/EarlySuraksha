18e0f603d24541ce3a7360a8a08300d92076201c
Sarthakjain714 <82282277+Sarthakjain714@users.noreply.github.com>
2022-08-19 11:58:52 +0530

    Revert "Dev"

--- app/src/main/java/com/example/earlysuraksha/register.java
+++ app/src/main/java/com/example/earlysuraksha/register.java
@@ -21,27 +21,26 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class register extends AppCompatActivity {
-    EditText signupemail, signuppassword, username, phonenumber;
+    EditText signupemail,signuppassword,username,phonenumber;
     TextView logintext;
     Button signup;
-    String emailsignup, passwordsignup, name, phnnumber;
+    String emailsignup,passwordsignup,name,phnnumber;
     myservice client;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-        signupemail = findViewById(R.id.emailidsignup);
-        signuppassword = findViewById(R.id.passwordsignup);
-        logintext = findViewById(R.id.logintext);
-        signup = findViewById(R.id.signupbutton);
-        username = findViewById(R.id.username);
-        phonenumber = findViewById(R.id.phonenumber);
+        signupemail=findViewById(R.id.emailidsignup);
+        signuppassword=findViewById(R.id.passwordsignup);
+        logintext=findViewById(R.id.logintext);
+        signup=findViewById(R.id.signupbutton);
+        username=findViewById(R.id.username);
+        phonenumber=findViewById(R.id.phonenumber);
 
         logintext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
                 startActivity(intent);
                 finish();
             }
@@ -49,46 +48,45 @@ public class register extends AppCompatActivity {
         signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                emailsignup = signupemail.getText().toString();
-                passwordsignup = signuppassword.getText().toString();
-                name = username.getText().toString();
-                phnnumber = phonenumber.getText().toString();
-                if (emailsignup.equals(" ")) {
+                emailsignup=signupemail.getText().toString();
+                passwordsignup=signuppassword.getText().toString();
+                name=username.getText().toString();
+                phnnumber=phonenumber.getText().toString();
+                if(emailsignup.equals(" ")){
                     Toast.makeText(register.this, "Enter a valid email id !!", Toast.LENGTH_SHORT).show();
                     return;
                 }
-                if (passwordsignup.equals(" ")) {
+                if(passwordsignup.equals(" ")){
                     Toast.makeText(register.this, "Password is mandatory !!", Toast.LENGTH_SHORT).show();
                     return;
                 }
-                if (username.equals(" ")) {
+                if(username.equals(" ")){
                     Toast.makeText(register.this, "Enter your name !!", Toast.LENGTH_SHORT).show();
                     return;
                 }
-                registeruser(emailsignup, passwordsignup, name, phnnumber);
+                registeruser(emailsignup,passwordsignup,name,phnnumber);
             }
         });
 
     }
 
-    public void registeruser(String emailsignup, String passwordsignup, String name, String phonenumber) {
-        client = retrofitclient.getInstance().create(myservice.class);
-        client.registerUser(emailsignup, name, passwordsignup, phonenumber).enqueue(new Callback<JsonObject>() {
+    public void registeruser(String emailsignup, String passwordsignup, String name,String phonenumber) {
+        client= retrofitclient.getInstance().create(myservice.class);
+        client.registerUser(emailsignup,name,passwordsignup,phonenumber).enqueue(new Callback<JsonObject>() {
             @Override
             public void onResponse(Call<JsonObject> call, Response<JsonObject> response) {
                 String answer = new Gson().toJson(response.body());
 
-                Log.d("resbody", "onResponse: " + answer + "\n" + answer.length());
+                Log.d("resbody", "onResponse: "+answer+"\n" +answer.length());
 
-                if (answer.length() > 100) {
+                if(answer.length()>100){
 
                     Toast.makeText(register.this, "New user created", Toast.LENGTH_SHORT).show();
-                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    Intent intent = new Intent(getApplicationContext(),MainActivity.class);
                     startActivity(intent);
                     finish();
                 }
             }
-
             @Override
             public void onFailure(Call<JsonObject> call, Throwable t) {
                 Toast.makeText(register.this, t.getMessage(), Toast.LENGTH_SHORT).show();
